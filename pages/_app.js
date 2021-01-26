3819773c41c3435afe8fe811acbc6958b3e583ec
Mauricio Girardi <maurigirarde@yahoo.com.br>
2021-01-26 15:31:40 -0300

    aula 01

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,46 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
+ * {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+
+  h1, h2, h3, h4, h5, h6  {
+    margin: 0;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  html, body {
+    min-height: 100vh;
+  }
+
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
